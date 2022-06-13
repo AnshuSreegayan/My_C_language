055dd49651fd39db7aa11d534cb287ee899dfb85
Anshu Kumar <98792386+AnshuSreegayan@users.noreply.github.com>
2022-06-13 20:45:17 +0530

    Add files via upload

---
+++ Q1_G_c.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int j=4;
+	(!j!=1?printf("Welcome"):printf("\nGood by"));
+	
+	return 0; 
+}
---
+++ Q1_G_d.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int j,i=65;
+	printf("\n %d %d ",j,i);
+	return 0;
+	
+}
