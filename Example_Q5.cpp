60d719ebb815bf4a149ee26dcea575ad45e5b491
Anshu Kumar <98792386+AnshuSreegayan@users.noreply.github.com>
2022-06-16 11:36:59 +0530

    Add files via upload

---
+++ Example_Q5.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int i,j ;
+//	for(i=10;i;i--)	
+//		printf("\n%d",i);
+//
+
+//	for(i<4;j=5;j=0)
+//	printf("%d",i);
+	
+	 for( i = 1; i <=10 ; printf ( "%d",i++ ) );
+			
+	return 0;
+}
